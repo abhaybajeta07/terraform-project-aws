64263491791bcbeeb287dc9e22d25047a95bbc9a
abhay_bajeta <abhaybajeta433@gmail.com>
2022-09-19 13:48:57 +0530

    key pairs

---
+++ backend-s3.tf
@@ -0,0 +1,7 @@
+terraform {
+  backend "s3" {
+    bucket = "terra-vprofile-07"
+    key = "terraform/backend"
+    region = "us-east-2"
+  }
+}
---
+++ vars.tf
@@ -0,0 +1,96 @@
+variable "AWS-REGION" {
+  default = "us-east-2"
+}
+
+variable "AMI" {
+  type = map()
+  default = {
+    us-east-2 = "ami-02f3416038bdb17fb"
+    us-east-1 = "ami-06489866022e12a14"
+    ap-south-1 = "ami-06489866022e12a14"
+  }
+}
+
+variable "PRIV_KEY_PATH" {
+  default = "vprofilekey"
+}
+
+variable "PUB_KEY_PATH" {
+  default = "vprofilekey.pub"
+}
+
+variable "USERNAME" {
+  default = "ubuntu"
+}
+
+variable "MYIP" {
+  default = "183.83.39.124/32"
+}
+
+variable "rmquser" {
+  default = "rabbit"
+}
+
+variable "rmqpass" {
+  default = "Gr33n@pple123456"
+}
+
+variable "dbuser" {
+  default = "admin"
+}
+
+variable "dbpass" {
+  default = admin123
+}
+
+variable "dbname" {
+  default = "accounts"
+}
+
+variable "instance_count" {
+  default = "1"
+}     #manipulate instance number
+
+variable "VPC_NAME" {
+  default = "vprofile-VPC"
+}
+
+variable "ZONE1" {
+  default = "us-east-2a"
+}
+
+variable "ZONE2" {
+  default = "us-east-2b"
+}
+
+variable "ZONE3" {
+  default = "us-east-2c"
+}
+
+variable "VpcCIDR" {
+  default = "172.21.0.0/16"
+}
+
+variable "PubSub1CIDR" {
+  default = "172.21.1.0/24"
+}
+
+variable "PubSub2CIDR" {
+  default = "172.21.2.0/24"
+}
+
+variable "PubSub3CIDR" {
+  default = "172.21.3.0/24"
+}
+
+variable "PrivSub1CIDR" {
+  default = "172.21.4.0/24"
+}
+
+variable "PrivSub2CIDR" {
+  default = "172.21.5.0/24"
+}
+
+variable "PrivSub3CIDR" {
+  default = "172.21.5.0/24"
+}
